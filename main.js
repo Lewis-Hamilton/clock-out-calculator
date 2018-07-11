14aa3a96cac1c029e89331a18328f7d8834ff0e9
Lewis-Hamilton <38916472+Lewis-Hamilton@users.noreply.github.com>
2018-07-11 11:18:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ function calc()
 {
   var f1 = document.getElementById('f1').value;
   var f2 = document.getElementById('f2').value;
+  var f3 = document.getElementById('f3').value;
 
-  var f3 = document.getElementById('f3').value = f1 +f2;
+  document.getElementById('f4').value = f3 - f2;
+  document.getElementById('f5').value = f4 + f1;
+  document.getElementById('f6').value = f5 + 8;
 }
+
+
+$(document).ready(function(){
+    $('.timepicker').timepicker();
+  });
