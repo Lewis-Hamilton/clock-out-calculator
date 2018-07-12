00f60e44e1a26625a4a31343495fb44d7896ffb0
Lewis-Hamilton <38916472+Lewis-Hamilton@users.noreply.github.com>
2018-07-12 12:12:11 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function calc()
 {
-
   var clockIn             = document.getElementById('clockIn').value;
   var clockOutMeal        = document.getElementById('clockOutMeal').value;
   var clockInMeal         = document.getElementById('clockInMeal').value;
@@ -33,30 +32,6 @@ function calc()
   var clockOutTime = clockOutHourFinal + ":" + clockOutMin;
 
   document.getElementById('hh').value = clockOutTime;
-
-
-  var clockIn      = document.getElementById('clockIn').value;
-  var clockOutMeal = document.getElementById('clockOutMeal').value;
-  var clockInMeal  = document.getElementById('clockInMeal').value;
-
-  var inHours      = clockIn.slice(0, 2);
-  var outMealHours = clockOutMeal.slice(0, 2);
-  var inMealHours  = clockInMeal.slice(0, 2);
-
-  var inMinutes      = clockIn.slice(3, 6);
-  var outMealMinutes = clockOutMeal.slice(3, 6);
-  var inMealMinutes  = clockInMeal.slice(3, 6);
-
-
-
-  // document.getElementById('clockOut').value = f6;
-
-  console.log(inMinutes);
-  console.log(outMealMinutes);
-  console.log(inMealMinutes);
-  console.log(inMealHours);
-  console.log(outMealHours);
-  console.log(inHours);
 
 }
 
